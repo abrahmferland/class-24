c2bfbdf834b89668beb2e5f430dab1702cbe031f
abrahmferland <82110663+abrahmferland@users.noreply.github.com>
2021-06-16 21:43:29 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    block1 = new Block(2,20)
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+
+    
+ 
+}
